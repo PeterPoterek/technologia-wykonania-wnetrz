14c765f7e87840303cbbde018c2865944f5b134c
Piotr Poterek <peterpoterek0@gmail.com>
2023-11-11 18:31:50 +0100

    css mobile refactor

--- src/main.js
+++ src/main.js
@@ -63,3 +63,44 @@ lazyImages.forEach((img) => {
 
 // simple lightbox
 const simpleLightbox = new SimpleLightbox(".gallery__container a", {});
+
+// mobile menu
+(() => {
+  const mobileMenu = document.querySelector("#mobile-menu");
+  const openMenuBtn = document.querySelector("#mobile-open");
+  const closeMenuBtn = document.querySelector("#mobile-close");
+  const mobileMenuLinks = document.querySelectorAll("#mobile-menu a");
+
+  const toggleMenu = () => {
+    const isMenuOpen = openMenuBtn.getAttribute("aria-expanded") === "true" || false;
+    openMenuBtn.setAttribute("aria-expanded", !isMenuOpen);
+    mobileMenu.classList.toggle("is-open");
+
+    if (isMenuOpen) {
+      document.body.style.overflow = "auto";
+    } else {
+      document.body.style.overflow = "hidden";
+    }
+  };
+
+  const closeMoblieMenu = () => {
+    mobileMenu.classList.remove("is-open");
+    openMenuBtn.setAttribute("aria-expanded", false);
+    document.body.style.overflow = "auto";
+  };
+
+  openMenuBtn.addEventListener("click", toggleMenu);
+  closeMenuBtn.addEventListener("click", toggleMenu);
+
+  mobileMenuLinks.forEach((link) => {
+    link.addEventListener("click", closeMoblieMenu);
+  });
+
+  window.matchMedia("(min-width: 768px)").addEventListener("change", (e) => {
+    if (!e.matches) return;
+    mobileMenu.classList.remove("is-open");
+    openMenuBtn.setAttribute("aria-expanded", false);
+
+    document.body.style.overflow = "auto";
+  });
+})();
